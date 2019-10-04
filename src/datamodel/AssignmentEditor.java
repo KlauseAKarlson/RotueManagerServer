2f8ed9edf95191538ba26c2fcedd9517d92a4016
KlauseAKarlson <KlauseAKarlson@gmail.com>
2019-10-04 11:20:35 -0400

    Refined constructors and access contorl

--- src/datamodel/AssignmentEditor.java
+++ src/datamodel/AssignmentEditor.java
@@ -14,12 +14,14 @@ public class AssignmentEditor {
 	/*
 	 * the only variables that should be by using this class are those in the RouteAssignment table. 
 	 * Everything else is used to contextualize information or provide a limited number of choices 
-	 * maintian database consistency wihtotu throwing exceptions
+	 * maintian database consistency without throwing exceptions
 	 */
 	private Hashtable<Integer,Route> RouteNames;
 	private Hashtable<Integer,Driver> Drivers;
-	public LinkedList<Assignment> Assignments;
+	private LinkedList<Assignment> Assignments;//used to save all assginments back into the database
+	private LinkedList<Assignment> Unassigned;//used to hold assignments without drivers in a side bar
 	private SQLconnection Database;
+	
 	public AssignmentEditor(SQLconnection db) throws SQLException {
 		Database=db;
 		//load route names, then Driver assignements
@@ -36,6 +38,7 @@ public class AssignmentEditor {
 		//load assignements into memory
 		Drivers =new Hashtable<Integer,Driver>();
 		Assignments =new LinkedList<Assignment> ();
+		Unassigned =new LinkedList<Assignment> ();
 		ResultSet DriverAssignments = Database.getData("SELECT * FROM DriverAssignment ORDER BY DriverID");
 		while (DriverAssignments.next())
 		{
@@ -46,13 +49,13 @@ public class AssignmentEditor {
 			Assignment a=null;
 			if (RouteID != 0)
 			{
-				a = new Assignment(RouteID);
+				a = new Assignment(RouteID);//Assignment ctor adds it to linked lists
 				//load and localize start/end times
 				Time start=DriverAssignments.getTime("StartTime");
 				a.Start = start==null?null:start.toLocalTime();
 				Time end =DriverAssignments.getTime("EndTime");
 				a.End=end==null?null:end.toLocalTime();
-			}
+			}//end if route not null
 			if (driverID != 0)
 			{
 				Driver d;
@@ -66,12 +69,31 @@ public class AssignmentEditor {
 				}else {
 					d=Drivers.get(driverID);
 				}
-				d.add(a);
-			}
-		}
-	}//end ctor
 
-	public class Route{
+				if (a!=null)
+					a.assign(d);
+			}//end if driver not null
+		}//end while loop
+	}//end ctor
+	
+	public void delete(Assignment a)
+	{
+		//removes the assignemnt from all containers, it will not be saved even if it persists in the GUI
+		Assignments.remove(a);
+		if (a.driver==null)
+		{
+			Unassigned.remove(a);
+		}else {
+			a.driver.Routes.remove(a);
+		}
+	}
+	public Assignment createAssignment(int RouteID)
+	{
+		return new Assignment(RouteID);
+	}
+	
+	
+ 	public class Route{
 		private String name;
 		private int RouteID;
 		public String getName() {return name;}
@@ -91,22 +113,16 @@ public class AssignmentEditor {
 			CDL_Exp= CDLE==null?null:CDLE.toInstant();
 		}
 		
-		public boolean CDLValid()
+		public boolean ExpiredCDL()
 		{
 			//return's false if the CTL is expired
-			return CDL_Exp.isAfter(Instant.now());
+			return CDL_Exp.isBefore(Instant.now());
 		}
 		public String getName() {return DriverName;}
 		public int getID() {return DriverID;}
 		public Instant getCDLExpirationDate() {return CDL_Exp;}
 		public LinkedList<Assignment> getRoutes(){return Routes;}
-		public void add(Assignment a)
-		{
-			if (a==null) return;//protect from null exceptions
-			a.remove();
-			a.driver=this;
-			Routes.add(a);
-		}
+
 	}///end driver
 	public class Assignment{
 		private int RouteID;
@@ -114,10 +130,13 @@ public class AssignmentEditor {
 		private Driver driver;
 		private LocalTime Start,End;
 
-		public Assignment(int routeID)
+		private Assignment(int routeID)
 		{
 			RouteID=routeID;
 			RouteName=RouteNames.get(RouteID).getName();
+			
+			Assignments.add(this);
+			Unassigned.add(this);
 		}
 		
 		public void remove()
@@ -126,8 +145,19 @@ public class AssignmentEditor {
 			if (driver!=null)
 			{
 				driver.Routes.remove(this);
+				Unassigned.add(this);
 			}
 			driver=null;
+		}
+		public void assign(Driver d)
+		{
+			if(driver != null)
+				driver.Routes.remove(this);
+			else
+				Unassigned.remove(this);
+			
+			driver=d;
+			driver.Routes.add(this);
 		}
 		
 		public int getID() {return RouteID;}
